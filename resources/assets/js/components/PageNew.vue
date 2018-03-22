8221cdc756a98f17f042288278e109d4b825209a
vagrant <vagrant@homestead>
2018-03-22 11:42:31 +0000

    111

---
+++ resources/assets/js/components/PageNew.vue
@@ -0,0 +1,18 @@
+<template>
+    <page :mess="mess"></page>
+</template>
+
+<script>
+    import Page from './Page'
+    export default {
+        components: {
+            Page
+        },
+        data () {
+            return {
+                mess: '111'
+            }
+        },
+
+    }
+</script>
